a8b12915b7d78b4aa1be20827f90246ddbd25a07
Daniel M <parodiathe@mail.ru>
2024-07-27 01:21:54 +0300

    configured spring security

---
+++ src/main/java/com/Makushev/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.Makushev.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "fdsfsdfdsfdsjlfsdfjsdkfjsdfjdsklfjekjrjwerjejfdsfjdsofudsfsdf";
+
+    public static final String JWT_HEADER="Authorization";
+
+}
