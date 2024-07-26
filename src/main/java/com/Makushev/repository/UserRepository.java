baff8afc3cbd7340a66852769b4b39a267a0d7d3
Daniel M <parodiathe@mail.ru>
2024-07-27 01:23:47 +0300

    added a User Repository

---
+++ src/main/java/com/Makushev/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Makushev.repository;
+
+import com.Makushev.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
