7d87f97baad5cca0bb111a2c609dc58f62754e21
Daniel M <parodiathe@mail.ru>
2024-07-27 01:25:23 +0300

    initial commit

---
+++ src/main/java/com/Makushev/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.Makushev.service;
+
+import com.Makushev.model.User;
+import com.Makushev.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService  implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user==null){
+            throw new UsernameNotFoundException(username);
+        }
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(), authorityList);
+    }
+}
