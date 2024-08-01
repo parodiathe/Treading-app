d10f9bded35cb9ca872243348881da0e78090510
Daniel M <parodiathe@mail.ru>
2024-08-01 23:49:44 +0300

    Added withdrawal entity for further implementation

---
+++ src/main/java/com/Makushev/model/Withdrawal.java
@@ -0,0 +1,26 @@
+package com.Makushev.model;
+
+import com.Makushev.domain.WithdrawalStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Withdrawal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private WithdrawalStatus status;
+
+    private Long amount;
+
+    @ManyToOne
+    private User user;
+
+    private LocalDateTime date = LocalDateTime.now();
+
+}
