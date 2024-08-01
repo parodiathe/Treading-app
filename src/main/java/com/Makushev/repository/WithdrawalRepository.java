50bd7579c7e14192c5ad08b87bc7695e95ff8470
Daniel M <parodiathe@mail.ru>
2024-08-02 00:29:03 +0300

    added withdrawal repository

---
+++ src/main/java/com/Makushev/repository/WithdrawalRepository.java
@@ -0,0 +1,12 @@
+package com.Makushev.repository;
+
+import com.Makushev.model.Withdrawal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WithdrawalRepository extends JpaRepository<Withdrawal, Long> {
+
+    List<Withdrawal> findByUserId(Long userId);
+
+}
