5851df74bf67202b6f6667e0f0ec8dad93313c73
Daniel M <parodiathe@mail.ru>
2024-08-02 23:00:15 +0300

    added a service annotation

--- src/main/java/com/Makushev/service/impl/PaymentDetailsServiceImpl.java
+++ src/main/java/com/Makushev/service/impl/PaymentDetailsServiceImpl.java
@@ -5,7 +5,9 @@ import com.Makushev.model.User;
 import com.Makushev.repository.PaymentDetailsRepository;
 import com.Makushev.service.PaymentDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PaymentDetailsServiceImpl implements PaymentDetailsService {
 
     PaymentDetailsRepository paymentDetailsRepository;
